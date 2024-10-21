d770985c3c9d771996a96e470a3fe4af6a7c03b8
nvnhat04 <nvnhat04324@gmail.com>
2024-10-21 11:23:39 +0700

    add: login api

--- src/controller/AccountController.js
+++ src/controller/AccountController.js
@@ -6,7 +6,7 @@ class AccountController {
     async signup(req, res) {
         const { username, display_name,  email, password, user_role } = req.body;
         try {
-           // Handle signup here
+            
         } catch (error) {
             res.status(500).send('Error: ' + error.message);
         }
@@ -15,6 +15,18 @@ class AccountController {
         const { username, password } = req.body;
         try{
             // Handle login here
+            const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+            if(user.rows.length === 0){
+                return res.status(400).send('User not found');
+            }
+            const validPassword = await bcrypt.compare(password, user.rows[0].password);
+            const token = jwt.sign({ id: user.rows[0].id, username: user.rows[0].username }, process.env.TOKEN_SECRET_KEY, {expiresIn: '24h'});
+            if(validPassword){
+                res.status(200).send({ auth: true, token: token });
+            }
+            else{
+                res.status(400).send('Invalid password');
+            }
         }
         catch(error){
             res.status(500).send('Error: ' + error.message);
