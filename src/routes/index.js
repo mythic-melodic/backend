ca0a13cebff8945dcccf68edad60982c699c8e32
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-11-10 13:33:23 +0700

    feat: add artist top tracks API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
-import accountRoute from './account.route.js';
+import accountRoute from "./account.route.js";
+import artistRoute from "./artist.route.js";
+
 function route(app) {
-    app.use('/api/account', accountRoute);
+  app.use("/api/account", accountRoute);
+  app.use("/api/artist", artistRoute);
 }
+
 export default route;
