a5db1598a4ccebacf36bd8a32a6071166bdc13bc
thuthuy <125456003+tthuy123@users.noreply.github.com>
2024-11-11 01:58:11 +0700

    feat: playlist route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import accountRoute from './account.route.js';
+import playlistRoute from './playlist.route.js';
 function route(app) {
-    app.use('/api/account', accountRoute);
+    app.use('/api/v1/account', accountRoute);
+    app.use('/api/v1/playlist', playlistRoute);
+
 }
 export default route;
