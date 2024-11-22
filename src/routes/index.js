42f6e9b7aed6299936bafb342fa880f05b80f5b2
nvnhat04 <nvnhat04324@gmail.com>
2024-11-22 23:36:16 +0700

    chore: delete unused drive API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import accountRoute from "./account.route.js";
 import playlistRoute from "./playlist.route.js";
 import artistRoute from "./artist.route.js";
-import driveRoute from "./drive.route.js";
 import trackRoute from "./track.route.js";
 function route(app) {
     app.use('/api/v1/account', accountRoute);
-    app.use('/api/v1/drive', driveRoute);
     app.use('/api/v1/artist', artistRoute);
     app.use('/api/v1/playlist', playlistRoute);
     app.use('/api/v1/track', trackRoute);
