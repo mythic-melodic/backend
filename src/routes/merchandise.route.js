37e92e2be731a89598d704600c7a39ad2936e7fe
Ngoc Linh Pham <121231072+ngoclinhphvm@users.noreply.github.com>
2024-11-29 09:28:26 +0700

    refactor: organize routes for merchandise (one per line)

--- src/routes/merchandise.route.js
+++ src/routes/merchandise.route.js
@@ -4,23 +4,9 @@ import tokenMiddleware from "../middlewares/token.middleware.js";
 import merchandiseController from "../controller/MerchandiseController.js";
 
 router.get("/all", merchandiseController.getAllMerchandise);
-
-router.post(
-  "/create",
-  tokenMiddleware.authenticateToken,
-  merchandiseController.createMerchandise
-);
-
-router.put(
-  "/:id",
-  tokenMiddleware.authenticateToken,
-  merchandiseController.updateMerchandise
-);
+router.post("/create",tokenMiddleware.authenticateToken,merchandiseController.createMerchandise);
+router.put("/:id", tokenMiddleware.authenticateToken,merchandiseController.updateMerchandise);
 router.get("/:id", merchandiseController.getMerchandiseById);
-router.delete(
-  "/delete/:id",
-  tokenMiddleware.authenticateToken,
-  merchandiseController.deleteMerchandise
-);
+router.delete("/delete/:id",tokenMiddleware.authenticateToken, merchandiseController.deleteMerchandise);
 
 export default router;
