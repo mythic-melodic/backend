b658f310302d9daff9da88c05621a6cc63a10d28
longsobad <nguyenlongdz2004@gmail.com>
2024-12-18 00:27:46 +0700

    :feat: add functions for merchandise handling

--- src/controller/MerchandiseController.js
+++ src/controller/MerchandiseController.js
@@ -133,6 +133,32 @@ class MerchandiseController {
     }
   }
 
+  async getTotalSold(req, res) {
+    try {
+      const merchandiseId = req.params.id;
+      const totalSold = await MerchandiseModel.getTotalSold(merchandiseId);
+      if (totalSold.message) {
+        return res.status(404).json({
+          success: false,
+          message: totalSold.message,
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Total sold fetched successfully.",
+        data: totalSold,
+      });
+    } catch (error) {
+      console.error("Error fetching total sold:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch total sold.",
+        error: error.message,
+      });
+    }
+  }
+
   async getNewArrivals(req, res) {
     try {
       // Gọi hàm từ model để lấy danh sách sản phẩm mới
@@ -146,11 +172,213 @@ class MerchandiseController {
       });
     } catch (error) {
       console.error("Error fetching new arrivals:", error);
-
-      // Xử lý lỗi, trả về mã trạng thái 500
       return res.status(500).json({
         success: false,
         message: "Failed to fetch new arrivals.",
+        error: error.message,
+      });
+    }
+  }
+
+  async getTrendingNow(req, res) {
+    try {
+      // Gọi hàm từ model để lấy danh sách sản phẩm trending
+      const data = await MerchandiseModel.getTrendingNow();
+
+      // Thành công, trả về danh sách
+      return res.status(200).json({
+        success: true,
+        message: "Trending products fetched successfully.",
+        data: data,
+      });
+    } catch (error) {
+      console.error("Error fetching trending products:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch trending products.",
+        error: error.message,
+      });
+    }
+  }
+
+  async getFavArtistStore(req, res) {
+    try {
+      const userId = req.params.id; 
+      const data = await MerchandiseModel.getFavArtistStore(userId);
+      if (!data || data.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "No merchandise found for the favorite artists.",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Favorite artist merchandise fetched successfully.",
+        data: data,
+      });
+    } catch (error) {
+      console.error("Error fetching favorite artist merchandise:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch favorite artist merchandise.",
+        error: error.message,
+      });
+    }
+  }
+  async getMerchandiseDetailById(req, res) {
+    try {
+      const merchandiseId = req.params.id;
+      const data = await MerchandiseModel.getMerchandiseDetailById(
+        merchandiseId
+      );
+      if (!data || data.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "Merchandise not found.",
+          data: null,
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Merchandise details fetched successfully.",
+        data: data,
+      });
+    } catch (error) {
+      console.error("Error fetching merchandise details:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch merchandise details.",
+        error: error.message,
+      });
+    }
+  }
+  async updateStock(req, res) {
+    try {
+      const merchandise_id = req.params.id;
+      const { stock } = req.body;
+      if (!merchandise_id || stock === undefined) {
+        return res.status(400).json({
+          success: false,
+          message: "Merchandise ID and stock value are required.",
+        });
+      }
+      const updatedMerchandise = await MerchandiseModel.updateStock(
+        merchandise_id,
+        stock
+      );
+
+      if (!updatedMerchandise) {
+        return res.status(404).json({
+          success: false,
+          message: "Merchandise not found.",
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Stock updated successfully.",
+        data: updatedMerchandise,
+      });
+    } catch (error) {
+      console.error("Error updating stock:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to update stock.",
+        error: error.message,
+      });
+    }
+  }
+  async getAllMerByArtist(req, res) {
+    try {
+      const artist_id = req.params.artist_id;
+      if (!artist_id) {
+        return res.status(400).json({
+          success: false,
+          message: "Artist ID is required.",
+        });
+      }
+      const merchandiseList = await MerchandiseModel.getAllMerByArtist(
+        artist_id
+      );
+
+      if (merchandiseList.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "No merchandise found for the specified artist.",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Merchandise fetched successfully.",
+        data: merchandiseList,
+      });
+    } catch (error) {
+      console.error("Error fetching merchandise:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch merchandise.",
+        error: error.message,
+      });
+    }
+  }
+  async getTopSellingMerchandiseByArtist(req, res) {
+    try {
+      const artist_id = req.params.artist_id;
+      if (!artist_id) {
+        return res.status(400).json({
+          success: false,
+          message: "Artist ID is required.",
+        });
+      }
+
+      const topSellingMerchandise =
+        await MerchandiseModel.getTopSellingMerchandiseByArtist(artist_id);
+
+      if (topSellingMerchandise.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "No merchandise found for the specified artist.",
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        message: "Top-selling merchandise fetched successfully.",
+        data: topSellingMerchandise,
+      });
+    } catch (error) {
+      console.error("Error fetching top-selling merchandise:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch top-selling merchandise.",
+        error: error.message,
+      });
+    }
+  }
+
+  async getMerchandiseBySearch(req, res) {
+    try {
+      const { searchTerm } = req.query;
+      if (!searchTerm) {
+        return res.status(400).json({
+          success: false,
+          message: "Search term is required.",
+        });
+      }
+      const searchResults = await MerchandiseModel.getMerchandiseBySearch(
+        searchTerm
+      );
+      return res.status(200).json({
+        success: true,
+        message: "Merchandise matching search term fetched successfully.",
+        data: searchResults, 
+      });
+    } catch (error) {
+      console.error("Error fetching merchandise by search:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to fetch merchandise.",
         error: error.message,
       });
     }
--- src/routes/merchandise.route.js
+++ src/routes/merchandise.route.js
@@ -5,9 +5,19 @@ import merchandiseController from "../controller/MerchandiseController.js";
 
 router.get("/all", merchandiseController.getAllMerchandise);
 router.get("/new-arrivals", merchandiseController.getNewArrivals);
+router.get("/trending-now", merchandiseController.getTrendingNow);
+router.get("/fav-artist-store/:id", merchandiseController.getFavArtistStore);
+router.get("/total-sold/:id", merchandiseController.getTotalSold);
+router.get("/detail/:id", merchandiseController.getMerchandiseDetailById);
 router.post("/create", tokenMiddleware.authenticateToken, merchandiseController.createMerchandise);
 router.put("/:id", tokenMiddleware.authenticateToken, merchandiseController.updateMerchandise);
+router.get("/search", merchandiseController.getMerchandiseBySearch);
 router.get("/:id", merchandiseController.getMerchandiseById);
 router.delete("/:id", tokenMiddleware.authenticateToken, merchandiseController.deleteMerchandise);
+router.put("/update-stock/:id", merchandiseController.updateStock);
+router.get("/all/:artist_id", merchandiseController.getAllMerByArtist);
+router.get("/top-selling/:artist_id", merchandiseController.getTopSellingMerchandiseByArtist);
+
+
 
 export default router;
